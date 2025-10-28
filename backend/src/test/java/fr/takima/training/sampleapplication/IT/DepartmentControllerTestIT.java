90693337488cb6cfc81247782c9f106c872eebf0
Sebastien Dors <dorssebastien@gmail.com>
2025-10-28 10:45:45 +0100

    Modification backend

--- backend/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
+++ backend/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
@@ -3,7 +3,6 @@ package fr.takima.training.sampleapplication.IT;
 import fr.takima.training.simpleapi.SimpleApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -15,13 +14,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@SpringBootTest(classes = {SimpleApiApplication.class})
+@SpringBootTest(classes = { SimpleApiApplication.class })
 class DepartmentControllerTestIT {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @Sql({"/InsertData.sql"})
+    @Sql({ "/InsertData.sql" })
     void testGetDepartmentByName() throws Exception {
         mockMvc.perform(get("/departments/ASI"))
                 .andExpect(status().isOk())
@@ -30,14 +29,14 @@ class DepartmentControllerTestIT {
     }
 
     @Test
-    @Sql({"/InsertData.sql"})
+    @Sql({ "/InsertData.sql" })
     void testGetNonExistingDepartmentByName() throws Exception {
         mockMvc.perform(get("/departments/NIMPORTEQUOI"))
                 .andExpect(status().isNotFound());
     }
 
     @Test
-    @Sql({"/InsertData.sql"})
+    @Sql({ "/InsertData.sql" })
     void testGetDepartmentStudentsByName() throws Exception {
         mockMvc.perform(get("/departments/ASI/students"))
                 .andExpect(status().isOk())
@@ -49,14 +48,14 @@ class DepartmentControllerTestIT {
     }
 
     @Test
-    @Sql({"/InsertData.sql"})
+    @Sql({ "/InsertData.sql" })
     void testGetNonExistingDepartmentStudentsByName() throws Exception {
         mockMvc.perform(get("/departments/NIMPORTEQUOI/students"))
                 .andExpect(status().isNotFound());
     }
 
     @Test
-    @Sql({"/InsertData.sql"})
+    @Sql({ "/InsertData.sql" })
     void testGetDepartmentCountByName() throws Exception {
         mockMvc.perform(get("/departments/ASI/count"))
                 .andExpect(status().isOk())
@@ -64,7 +63,7 @@ class DepartmentControllerTestIT {
     }
 
     @Test
-    @Sql({"/InsertData.sql"})
+    @Sql({ "/InsertData.sql" })
     void testGetNonExistingDepartmentCountsByName() throws Exception {
         mockMvc.perform(get("/departments/NIMPORTEQUOI/count"))
                 .andExpect(status().isNotFound());
